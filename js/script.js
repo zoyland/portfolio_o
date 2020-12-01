e26d1932206c3595620f9bfbf6bc33b2bfed2741
zoy <74699196+zoyland@users.noreply.github.com>
2020-12-02 01:19:42 +0900

    20201202

--- js/script.js
+++ js/script.js
@@ -14,14 +14,15 @@ var swiper = new Swiper('.swiper-container', {
       fullscreen.classList.toggle("light");
   })
   // dark mode toggle
+
   $('.circle').click(function(){
     $(this).animate({'left':'50%'});
   });
   // dark mode toggle move to right 
 
-  $('#other1 .area .other_wrap .info ul li p').mouseover(function(){
-    $(this).siblings('img').effect("shake", { times:3 }, 100);
-  });
+  // $('#other1 .area .other_wrap .info ul li p').mouseover(function(){
+  //   $(this).siblings('img').effect("shake", { times:3 }, 100);
+  // });
   // #other icon shaking
 
   $('.base_hover').hover(function(){
@@ -33,6 +34,34 @@ var swiper = new Swiper('.swiper-container', {
   });
   // h1 
 
+  var $pfarea = $('.pfarea').find('ul');
+    $('.goingleft').click(function(){
+        $pfarea.css("width" , "200%");
+        $pfarea.css("height" , "80%");
+
+        $pfarea.animate({"left": "-35%"}, 1000, function(){
+            $(this).append("<li>" + $(this).find("li:first").html() + "</li>");
+            $(this).find("li:first").remove();
+            $(this).css("left", "0%");
+            $(this).find("li").removeClass('on');
+            $(this).find("li:nth-child(2)").addClass('on');
+        });
+    });
+    $('.goingright').click(function(){
+        $pfarea.css("width" , "200%");
+        $pfarea.css("height" , "80%");
+
+        $pfarea.animate({"right": "-35%"}, 1000, function(){
+            $(this).append("<li>" + $(this).find("li:last").html() + "</li>");
+            console.log("<li>" + $(this).find("li:last").html());
+            $(this).find("li:last").remove();
+            $(this).css("right", "0%");
+            $(this).find("li").removeClass('on');
+            $(this).find("li:nth-child(4)").addClass('on');
+        });
+    });
+
+  
 // $(function(){
 //     $("html, body").animate({ scrollTop: 0 }, "slow"); 
 // });
